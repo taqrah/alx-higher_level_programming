bbc698e9dd33de58823bfd2e947de2f75b655659
Dom-iha <106284691+Dom-iha@users.noreply.github.com>
2022-12-13 11:36:09 +0100

    python-hello_world

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,26 +1,42 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a singly linked list has a cycle in it
- *
- * @list - pointer to the list
- * Return: 0 if there is no cycle, 1 if there is a cycle
+ * check_cycle - checks if a singly linked list has
+ * a cycle in it
+ * @list: pointer to the list
+ * Return: 0 if there is no cycle,
+ * 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *tortoise, *hare;
+	listint_t *p2;
+	listint_t *prev;
 
-	tortoise = list;
-	hare = list;
-
-	while (tortoise && hare)
+	p2 = list;
+	prev = list;
+	while (list && p2 && p2->next)
 	{
-		if (hare->next == NULL)
-			return (0);
-		tortoise = tortoise->next;
-		hare = hare->next->next;
-		if (tortoise == hare)
+		list = list->next;
+		p2 = p2->next->next;
+
+		if (list == p2)
+		{
+			list = prev;
+			prev =  p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != list && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == list)
+					break;
+
+				list = list->next;
+			}
 			return (1);
+		}
 	}
 
 	return (0);
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -5,9 +5,9 @@
 
 /**
  * struct listint_s - singly linked list
- *
  * @n: integer
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
  * for Holberton project
  */
